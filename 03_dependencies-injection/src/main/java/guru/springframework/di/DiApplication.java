c84b64f69ec5cf21e0d21c4d811d98a2ec9e7e8d
amarchino <marchino.alessandro@gmail.com>
2020-12-27 22:42:13 +0100

    Lezione 32

--- 03_dependencies-injection/src/main/java/guru/springframework/di/DiApplication.java
+++ 03_dependencies-injection/src/main/java/guru/springframework/di/DiApplication.java
@@ -2,12 +2,18 @@ package guru.springframework.di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import guru.springframework.di.controllers.MyController;
 
 @SpringBootApplication
 public class DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
