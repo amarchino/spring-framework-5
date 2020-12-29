bfd00db7893a2870e100888dc7e50c025289b7c1
amarchino <marchino.alessandro@gmail.com>
2020-12-29 17:23:52 +0100

    Lezione 37

--- 03_dependencies-injection/src/main/java/guru/springframework/di/DiApplication.java
+++ 03_dependencies-injection/src/main/java/guru/springframework/di/DiApplication.java
@@ -14,9 +14,10 @@ public class DiApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
+		
+		System.out.println("------ Primary Bean");
 		MyController myController = (MyController) ctx.getBean("myController");
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+		System.out.println(myController.sayHello());
 		
 		System.out.println("------ Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
--- 03_dependencies-injection/src/main/java/guru/springframework/di/controllers/MyController.java
+++ 03_dependencies-injection/src/main/java/guru/springframework/di/controllers/MyController.java
@@ -2,10 +2,18 @@ package guru.springframework.di.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.di.services.GreetingService;
+
 @Controller
 public class MyController {
+	
+	private final GreetingService greetingService;
+
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+	
     public String sayHello() {
-        System.out.println("Hello World!!!");
-        return "Hi folks!";
+        return greetingService.sayGreeting();
     }
 }
