799ac8dd436e5cbd042b6974ff9b824927b195e9
amarchino <marchino.alessandro@gmail.com>
2021-01-01 19:06:10 +0100

    Lezione 62. Closes #5

---
+++ _sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+	@Override
+	public Owner save(Owner object) {
+		return save(object.getId(), object);
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		return null;
+	}
+
+}
---
+++ _sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.VetService;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+	@Override
+	public Vet save(Vet object) {
+		return save(object.getId(), object);
+	}
+
+}
