e517bd74c250557618814b4698a22ba9b762539c
amarchino <marchino.alessandro@gmail.com>
2021-01-02 19:51:00 +0100

    Lezione 90

--- 03_dependencies-injection/src/main/java/guru/springframework/di/config/PropertyConfig.java
+++ 03_dependencies-injection/src/main/java/guru/springframework/di/config/PropertyConfig.java
@@ -4,16 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 import guru.springframework.di.examplebeans.FakeDataSource;
 import guru.springframework.di.examplebeans.FakeJmsBroker;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
-@PropertySource("classpath:jms.properties")
 public class PropertyConfig {
 	
 	@Autowired Environment env;
@@ -26,12 +22,6 @@ public class PropertyConfig {
 	@Value("${guru.jms.password}") String jmsPassword;
 	@Value("${guru.jms.url}") String jmsUrl;
 
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		return propertySourcesPlaceholderConfigurer;
-	}
-	
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
