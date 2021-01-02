febbcb43dd6bf8018ffe3ebd5639197862f85e67
amarchino <marchino.alessandro@gmail.com>
2021-01-02 19:36:06 +0100

    Lezione 88

--- 03_dependencies-injection/src/main/java/guru/springframework/di/config/PropertyConfig.java
+++ 03_dependencies-injection/src/main/java/guru/springframework/di/config/PropertyConfig.java
@@ -1,16 +1,20 @@
 package guru.springframework.di.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import guru.springframework.di.examplebeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+	
+	@Autowired Environment env;
 	
 	@Value("${guru.username}") String user;
 	@Value("${guru.password}") String password;
@@ -25,7 +29,7 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(user);
+		fakeDataSource.setUser(env.getProperty("USERNAME", user));
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
