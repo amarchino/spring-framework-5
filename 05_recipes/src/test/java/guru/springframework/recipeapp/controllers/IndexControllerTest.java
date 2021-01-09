c6ee40372a7822ecbd0c3a87761babc00382dd6f
amarchino <marchino.alessandro@gmail.com>
2021-01-09 15:17:27 +0100

    Lezione 177

--- 05_recipes/src/test/java/guru/springframework/recipeapp/controllers/IndexControllerTest.java
+++ 05_recipes/src/test/java/guru/springframework/recipeapp/controllers/IndexControllerTest.java
@@ -1,6 +1,9 @@
 package guru.springframework.recipeapp.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,6 +14,8 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import guru.springframework.recipeapp.domain.Recipe;
@@ -18,15 +23,23 @@ import guru.springframework.recipeapp.service.RecipeService;
 
 class IndexControllerTest {
 	
-	private IndexController indexController;
+	private IndexController controller;
 	@Mock private RecipeService recipeService;
 	@Mock private Model model;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		try (AutoCloseable ac = MockitoAnnotations.openMocks(this)) {
-			indexController = new IndexController(recipeService);
+			controller = new IndexController(recipeService);
 		}
+	}
+	
+	@Test
+	void testMockMvc() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(get("/"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("index"));
 	}
 
 	@Test
@@ -42,7 +55,7 @@ class IndexControllerTest {
 		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 		
 		// When
-		String result = indexController.getIndexPage(model);
+		String result = controller.getIndexPage(model);
 		
 		// Then
 		assertEquals("index", result);
