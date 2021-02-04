e79a2eeff6a64cf1dfaffb92dfc1af6092bb049c
amarchino <marchino.alessandro@gmail.com>
2021-02-04 22:12:26 +0100

    Lezione 328

---
+++ 08_reactive-example/src/test/java/guru/springframework/reactiveexample/ReactiveExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.reactiveexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
